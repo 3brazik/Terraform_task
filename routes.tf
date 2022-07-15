cc8fccbe49ba3a4217d592c197ee471283ace919
3brazik <mohamedabrazek936@gmail.com>
2022-07-15 22:47:20 +0200

    test

--- routes.tf
+++ routes.tf
@@ -31,7 +31,6 @@ resource "aws_route_table" "private" {
 
 }
 
-
 resource "aws_route_table_association" "private1" {
   subnet_id      = aws_subnet.private1.id
   route_table_id = aws_route_table.private.id
